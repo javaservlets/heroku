feb2631e2e3f6ebce43055d34308b87b995f69b8
javaservlets <javaservlets@hotmail.com>
2018-09-03 16:46:47 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-var http = require('http');
-
-http.createServer(function (request, response) {
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.end('Hello World\n');
-}).listen(8080);
-
-console.log('Server started!');
+const http = require('http');
+const PORT = process.env.PORT || 5000;
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
+});
+server.listen(PORT, () => {
+  console.log(`Server running on ${PORT}/`);
+});
